2e76d806108928e40fe7a058401b59c26e1ea963
Andrew Yaremenko <esdesuagk@gmail.com>
2023-07-01 12:10:30 +0300

    Users migration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
-{
+{    
+    /**
+     * Home page
+     *
+     * @return view
+     */
     public function index(){
         return view('main');
     }
