d6a2d9d96643e3c344d9139a846fa022dab76056
Andrew Yaremenko <esdesuagk@gmail.com>
2023-07-01 11:44:42 +0300

    HomeController, Bootstrap 5, Route

---
+++ resources/views/main.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+@section('content')
+    <section class="py-5 text-center container">
+        <div class="row py-lg-5">
+            <div class="col-lg-6 col-md-8 mx-auto">
+                <h1 class="fw-light">NexusNet</h1>
+                <p class="lead text-body-secondary">Discover the power of NexusNet, a simple and intuitive social network
+                    designed to connect people worldwide. Share your thoughts, ideas, and experiences with a vibrant
+                    community. Stay informed, make new friends, and explore a world of possibilities.</p>
+            </div>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/partials/navigation.blade.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <div class="container-fluid">
+        <span class="navbar-brand ms-2">NexusNet</span>
+        <div class="navbar-nav ms-auto">
+            <button class="btn btn-outline-success me-2">Sign up</button>
+            <button class="btn btn-outline-success me-2">Login</button>
+        </div>
+    </div>
+</nav>
+
